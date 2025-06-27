051c801301b740521a4ccd1f339818d3eec85665
kettek <kts@kettek.net>
2025-06-26 22:36:05 -0700

    Add kind debuggie

--- prototype/id/main.go
+++ prototype/id/main.go
@@ -10,12 +10,16 @@ func main() {
 	var id world.SchlubID
 	fmt.Println("fam", id.FamilyID())
 	fmt.Println("schl", id.SchlubID())
+	fmt.Println("kind", id.KindID())
 	nextSchlub := id.NextSchlub().NextSchlub()
+	nextSchlub.SetKindID(int(world.SchlubKindMonk))
 	fmt.Println("fam", nextSchlub.FamilyID())
 	fmt.Println("schl", nextSchlub.SchlubID())
+	fmt.Println("kind", nextSchlub.KindID())
 	nextFamily := nextSchlub.NextFamily().NextFamily().NextFamily()
 	fmt.Println("fam", nextFamily.FamilyID())
 	fmt.Println("schl", nextFamily.SchlubID())
+	fmt.Println("kind", nextFamily.KindID())
 
 	id = nextFamily.NextSchlub()
 	id.SetKindID(4)
